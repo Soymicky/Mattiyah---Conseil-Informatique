8bc6a7899c6dc2084140917a3557cc8ec7a61ed1
mickaellaSamba <mickaellasamx@gmail.com>
2025-06-03 17:21:19 +0200

    Avant modification du calendrier en twig

--- src/Controller/PrendreRDVController.php
+++ src/Controller/PrendreRDVController.php
@@ -1,20 +1,28 @@
 <?php
+
 namespace App\Controller;
 
-use App\Entity\Services;
+use App\Entity\NiveauService;
 use App\Entity\RendezVous;
+use App\Entity\Services;
+use App\Repository\NiveauServiceRepository;
+use App\Repository\ServicesRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Security;
 
 final class PrendreRDVController extends AbstractController
 {
-    #[Route('prendre_rdv', name: 'prendre_rdv')]
-    public function index(EntityManagerInterface $em): Response
-    {
+    #[Route('/prendre_rdv', name: 'prendre_rdv')]
+    public function index(
+        EntityManagerInterface $em,
+        ServicesRepository $servicesRepository,
+        NiveauServiceRepository $niveauServiceRepository
+    ): Response {
         $utilisateur = $this->getUser();
         $dejaRDV = false;
 
@@ -27,69 +35,81 @@ final class PrendreRDVController extends AbstractController
             }
         }
 
-        $services = $em->getRepository(Services::class)->findAll();
-        $typesOffre = $em->getRepository(Services::class)->findUniqueTypesService();
+        $services = $servicesRepository->findAll(); 
+        $niveauService = $niveauServiceRepository->findAll();
 
-        return $this->render('/back/prendre_rdv.html.twig', [
+        return $this->render('back/prendre_rdv.html.twig', [
             'controller_name' => 'PrendreRDVController',
             'services' => $services,
-            'typesOffre' => $typesOffre,
-            'dejaRDV' => $dejaRDV, // Passer la variable (plus courte) au template
+            'niveauService' => $niveauService,
+            'dejaRDV' => $dejaRDV,
         ]);
     }
 
     #[Route('/enregistrer_rdv', name: 'enregistrer_rdv', methods: ['POST'])]
-    public function enregistrerRdv(Request $request, EntityManagerInterface $em, LoggerInterface $logger): Response
-    {
-        
+    public function enregistrerRdv(
+        Request $request,
+        EntityManagerInterface $em,
+        LoggerInterface $logger,
+        ServicesRepository $servicesRepository,
+        NiveauServiceRepository $niveauServiceRepository
+    ): Response {
         $logger->info('Tentative d\'enregistrement de rendez-vous...');
-        $allValues = $request->request->all();
-        // $date = $allValues['date'];
-        $date = NULL;
-        $heure = $allValues['heure'];
-        $postServices = $allValues['services'];
-        $typeServiceNom = $allValues['typeService'];
-        // $heure = $request->request->get('heure');
-        // $services = $request->request->get('services');
-        //$typeServiceNom = $request->request->get('typeService'); // Récupération de la valeur du champ 'typeService' du formulaire
 
-        // --- AJOUTEZ CES LIGNES POUR LE DÉBOGAGE SI NÉCESSAIRE ---
-        // var_dump( $heure, $services, $typeServiceNom);
-        // dd($date, $heure, $serviceNom, $typeServiceNom); // Vous pouvez utiliser dd() à la place de dump() pour arrêter l'exécution après l'affichage
+        $date = $request->request->get('date');
+        $heure = $request->request->get('heure');
+        $Service = $request->request->all('services');
+        $niveauService = $request->request->get('niveauService'); 
 
-        // if (!$date || !$heure || empty($serviceNom) || empty($typeServiceNom)) {
-        //     $this->addFlash('danger', 'Tous les champs sont requis.');
-        //     return $this->redirectToRoute('prendre_rdv');
-        // }
-
-        foreach($postServices as $serv ) {
-            $services[] = $em->getRepository(Services::class)->find($serv);
+        if (!$date || !$heure || empty($postServiceIds) || !$niveauServiceId) {
+            $this->addFlash('danger', 'Tous les champs sont requis.');
+            return $this->redirectToRoute('prendre_rdv');
         }
-        // if (!$service) {
-        //     $this->addFlash('danger', 'Le service sélectionné est invalide.');
-        //     return $this->redirectToRoute('prendre_rdv');
-        // }
-        var_dump($services);
-        die;
+        
         try {
             $datetime = new \DateTime("$date $heure");
+            $datetime->setTime($datetime->format('H'), $datetime->format('i'), 0);
         } catch (\Exception $e) {
             $this->addFlash('danger', 'Le format de la date ou de l\'heure est invalide.');
+            $logger->error('Erreur de format de date/heure: ' . $e->getMessage());
             return $this->redirectToRoute('prendre_rdv');
         }
 
         $rdv = new RendezVous();
-        $rdv->setDateRDV($datetime);
-        foreach($services as $serv ) {
-            $rdv->addService($serv);
+
+        foreach ($Service as $serviceId) {
+            $service = $servicesRepository->find($serviceId);
+            if ($service) {
+                $rdv->addService($service);
+            } else {
+                $this->addFlash('danger', "Le service avec l'ID $serviceId est invalide.");
+                return $this->redirectToRoute('prendre_rdv');
+            }
         }
-        $rdv->setTypeService($typeServiceNom); // Utilisation correcte de la méthode setTypeService() et de la variable $typeServiceNom
+        
+        $niveauService = $niveauServiceRepository->find($niveauServiceId);
+        if (!$niveauService) {
+            $this->addFlash('danger', 'Le niveau de service sélectionné est invalide.');
+            return $this->redirectToRoute('prendre_rdv');
+        }
+        // CORRECTION MAJEURE ICI : Utilisation de setTypeService() au lieu de setNiveauService()
+        // Ceci suppose que votre entité RendezVous a une propriété $typeService pour la relation NiveauService
+        $rdv->setTypeService($niveauService); 
+
+        $rdv->setDateRDV($datetime);
         $rdv->setUtilisateur($this->getUser());
+        $rdv->setStatut('en attente');
 
-        $em->persist($rdv);
-        $em->flush();
+        try {
+            $em->persist($rdv);
+            $em->flush();
 
-        $this->addFlash('success', 'Rendez-vous enregistré avec succès !');
+            $this->addFlash('success', 'Rendez-vous enregistré avec succès !');
+        } catch (\Exception | \Throwable $e) { 
+            $this->addFlash('danger', 'Une erreur est survenue lors de l\'enregistrement du rendez-vous. Veuillez réessayer.');
+            $logger->error('Erreur lors de l\'enregistrement du RDV: ' . $e->getMessage(), ['exception' => $e]);
+        }
+
         return $this->redirectToRoute('prendre_rdv');
     }
 }
